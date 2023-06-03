c04430018d20efbe04da9b6407665f8196c83e54
LiYuqi <49839855+LIyvqi@users.noreply.github.com>
2023-06-03 19:20:24 +0800

    Update PathFinder.java

--- SNCS/utils/graphUtils/PathFinder.java
+++ SNCS/utils/graphUtils/PathFinder.java
@@ -2,9 +2,7 @@ package utils.graphUtils;
 
 import java.util.*;
 
-/*
- *  以s作为起点，通过广度优先算法来寻找从s到图上任意一个顶点的路径
- */
+
 public class PathFinder {
 
     // prev[v] = previous vertex on shortest path from s to v
@@ -55,18 +53,4 @@ public class PathFinder {
         return path;
     }
 
-
-    public static void main(String[] args) {
-        String filename  = "D:\\JavaProject\\TBCSH_TEST\\src\\resultData\\queryGraph-4-1.txt";
-        String delimiter = " ";
-        Graph G = new Graph(filename, delimiter);
-        System.out.println(G);
-        String s = "0";
-        PathFinder pf = new PathFinder(G, s);
-        System.out.println("图构造完毕");
-        String t="1";
-        for (String vid:pf.pathTo(t)) System.out.print(vid+"-");
-        System.out.println();
-        System.out.println(t+"到"+s+"的路径长度是："+pf.distanceTo(t));
-    }
 }
