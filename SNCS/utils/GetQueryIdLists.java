9b11eb36b3938ce948ec8e119f18e321deff8e40
LiYuqi <49839855+LIyvqi@users.noreply.github.com>
2023-06-03 19:22:02 +0800

    Update GetQueryIdLists.java

--- SNCS/utils/GetQueryIdLists.java
+++ SNCS/utils/GetQueryIdLists.java
@@ -11,7 +11,6 @@ public class GetQueryIdLists {
         Map<Integer,Integer> map=new TreeMap<Integer, Integer>(
                 new Comparator<Integer>(){
                     public int compare(Integer obj1,Integer obj2){
-                        //按照降序排列的
                         return obj2.compareTo(obj1);
                     }
                 });
@@ -56,21 +55,4 @@ public class GetQueryIdLists {
         return queryIdList;
     }
 
-    //  编写了一个数据集用来测试上述代码
-//    public static void main(String[] args) {
-//        int[][] graph=new int[8][];
-//        graph[0]=new int[]{2,0,3,1};
-//        graph[1]=new int[]{3,2,4,3,5,4};
-//        graph[2]=new int[]{0,0,6,5};
-//        graph[3]=new int[]{0,1,1,2,6,6,7,9};
-//        graph[4]=new int[]{1,3,6,7};
-//        graph[5]=new int[]{1,4,6,8,7,10};
-//        graph[6]=new int[]{2,5,3,6,4,7,5,8};
-//        graph[7]=new int[]{3,9,5,10};
-//
-//        int[] vertexType=new int[]{1,1,2,2,2,2,3,3};
-//        int[] edgeType=new int[]{1,1,1,1,1,1,1,1,1,1,1};
-//
-//        MetaPath metaPath=new MetaPath(new int[]{1,2,3,2,1},new int[]{1,1,1,1});
-//    }
 }
