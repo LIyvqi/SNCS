33c22d5cba8a693b41c631a28b8ec49007717d59
LiYuqi <49839855+LIyvqi@users.noreply.github.com>
2023-06-03 19:16:57 +0800

    Update AnalyzePathSim.java

--- SNCS/utils/graphUtils/AnalyzePathSim.java
+++ SNCS/utils/graphUtils/AnalyzePathSim.java
@@ -15,7 +15,6 @@ public class AnalyzePathSim {
         this.edgeType=edgeType;
     }
 
-    //pGraph 用来计算图中任意两个顶点间的路径实例数
     public Map<Integer,Map<Integer, Integer>> batchBuildForPathSim(Set<Integer> bcoreSet, MetaPath queryMPath){
         Map<Integer,Map<Integer,Integer>> pGraph=new HashMap<>();
         for (int vid:bcoreSet){
@@ -28,8 +27,7 @@ public class AnalyzePathSim {
         return pGraph;
     }
 
-    //用来寻找vid到其它id的p-nbr路径实例
-    //相似性是允许边重复的，无所谓，对我们元结构来说也适用这个算法
+
     private Map<Integer, Integer> searchVnbPath(int vid,MetaPath queryMPath) {
         Map<Integer, Integer> pathNum=new HashMap<>();
         Queue<Integer> queue=new LinkedList<>();
@@ -40,8 +38,7 @@ public class AnalyzePathSim {
         for (int i = 0; i < queryMPath.pathLen; i++) {
             int targetV=queryMPath.vertex[i+1],targetE=queryMPath.edge[i];
             int layerLen=queue.size();
-            //通过广度优先搜索来批量的寻找从vid
-            //其实这里面是一个广度优先加动态规划
+
             for (int j = 0; j < layerLen; j++) {
                 int vertexId=queue.poll();
                 int paths=pathNum.get(vertexId);
@@ -63,19 +60,4 @@ public class AnalyzePathSim {
         return vnbPath;
     }
 
-//      用来测试计算顶点相似性的代码
-//    public static void main(String[] args) {
-//        Test2 test2=new Test2();
-//        AnalyzePathSim analyzePathSim=new AnalyzePathSim(test2.graph, test2.vertexType, test2.edgeType);
-//        Set<Integer> curSet=new HashSet<>();
-//        curSet.add(0);
-//        curSet.add(1);
-//        curSet.add(2);
-//        curSet.add(3);
-//        System.out.println(analyzePathSim.batchBuildForPathSim(curSet, test2.qeuryMetaPath));
-//
-//        PathSimBaselineFloat pathSimBaselineFloat=new PathSimBaselineFloat(test2.graph, test2.vertexType, test2.edgeType, test2.qeuryMetaPath, "D:\\JavaProject\\TBCSH_TEST\\src\\resultData\\queryGraph-4-1.txt");
-//        System.out.println(pathSimBaselineFloat.query(0, test2.qeuryMetaPath, 1,0.2));
-//
-//    }
 }
